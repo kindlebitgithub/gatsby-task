3f04d5609d2a4952a523f31c291e73d19305846c
kindlebitgithub <kindlebitgithub@gmail.com>
2023-06-28 16:24:04 +0530

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,72 @@
+// /**
+//  * Configure your Gatsby site with this file.
+//  *
+//  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+//  */
+
+// /**
+//  * @type {import('gatsby').GatsbyConfig}
+//  */
+// const { resolve } = require('path');
+// module.exports = {
+//   siteMetadata: {
+//     title: `Gatsby Default Starter`,
+//     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+//     author: `@gatsbyjs`,
+//     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+//   },
+//   plugins: [
+//     `gatsby-plugin-image`,
+//     {
+//       resolve: `gatsby-source-filesystem`,
+//       options: {
+//         name: `images`,
+//         path: `${__dirname}/src/images`,
+//       },
+//     },
+//     `gatsby-transformer-sharp`,
+//     `gatsby-plugin-sharp`,
+//     {
+//       resolve: `gatsby-plugin-manifest`,
+//       options: {
+//         name: `gatsby-starter-default`,
+//         short_name: `starter`,
+//         start_url: `/`,
+//         background_color: `#663399`,
+//         display: `minimal-ui`,
+//         icon: `src/images/gatsby-icon.png`, 
+//       },
+//     },
+//     {
+//       resolve: `gatsby-source-wordpress`,
+//       options: {
+//         url: `http://getsby.kindlebit.com/graphql`, 
+        
+//         // url: `https://tgibfnov2022.click/graphql`, 
+//         schema: {
+//           perPage: 20,
+//           requestConcurrency: 5,
+//           previewRequestConcurrency: 2,
+//         },
+//         develop: {
+//           hardCacheMediaFiles: true,
+//         },
+//         type: {
+//           MediaItem: {
+//             localFile: {
+//               requestConcurrency: 10,
+//             },
+//           },
+//         },
+//         debug: {
+//           graphql: {
+//             showQueryVarsOnError: true,
+//           },
+//         },
+//       },
+//     },
+//   ],
+// };
 /**
  * Configure your Gatsby site with this file.
  *
@@ -34,15 +103,14 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, 
+        icon: `src/images/gatsby-icon.png`,
       },
     },
     {
       resolve: `gatsby-source-wordpress`,
       options: {
-        url: `http://getsby.kindlebit.com/graphql`, 
-        
-        // url: `https://tgibfnov2022.click/graphql`, 
+        url: `https://tgibfnov2022.click/graphql`, // Update the URL here
+
         schema: {
           perPage: 20,
           requestConcurrency: 5,
@@ -60,7 +128,7 @@ module.exports = {
         },
         debug: {
           graphql: {
-            showQueryVarsOnError: true,
+            showQueryVarsOnError: false,
           },
         },
       },
--- src/templates/wp_page.js
+++ src/templates/wp_page.js
@@ -1,11 +1,38 @@
+// import React from 'react';
+
+// const PageTemplate = ({ data }) => {
+//   const page = data.wordpressPage;
+
+//   return (
+//     <div>
+//       <h1>{page.title}</h1>
+//       <div dangerouslySetInnerHTML={{ __html: page.content }} />
+//     </div>
+//   );
+// };
+
+// export const query = graphql`
+//   query($slug: String!) {
+//     wordpressPage(slug: { eq: $slug }) {
+//       title
+//       content
+//     }
+//   }
+// `;
+
+// export default PageTemplate;
+
 import React from 'react';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 const PageTemplate = ({ data }) => {
   const page = data.wordpressPage;
+  const featuredImage = getImage(page.featuredImage);
 
   return (
     <div>
       <h1>{page.title}</h1>
+      {featuredImage && <GatsbyImage image={featuredImage} alt="Featured" />}
       <div dangerouslySetInnerHTML={{ __html: page.content }} />
     </div>
   );
@@ -16,6 +43,17 @@ export const query = graphql`
     wordpressPage(slug: { eq: $slug }) {
       title
       content
+      featuredImage {
+        localFile {
+          childImageSharp {
+            gatsbyImageData(
+              width: 800
+              placeholder: BLURRED
+              formats: [AUTO, WEBP, AVIF]
+            )
+          }
+        }
+      }
     }
   }
 `;
