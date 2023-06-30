2a7d68d688e3ba64fccc56748668d86293733a5d
Mintu Kumar <mintu.kumar@kindlebit.net>
2023-06-30 18:41:20 +0530

    create gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,82 +7,6 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
-// module.exports = {
-//   siteMetadata: {
-//     title: `Gatsby Default Starter`,
-//     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-//     author: `@gatsbyjs`,
-//     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
-//   },
-//   plugins: [
-//     `gatsby-plugin-image`,
-//     {
-//       resolve: `gatsby-source-filesystem`,
-//       options: {
-//         name: `images`,
-//         path: `${__dirname}/src/images`,
-//       },
-//     },
-//     `gatsby-transformer-sharp`,
-//     `gatsby-plugin-sharp`,
-//     {
-//       resolve: `gatsby-plugin-manifest`,
-//       options: {
-//         name: `gatsby-starter-default`,
-//         short_name: `starter`,
-//         start_url: `/`,
-//         background_color: `#663399`,
-//         // This will impact how browsers show your PWA/website
-//         // https://css-tricks.com/meta-theme-color-and-trickery/
-//         // theme_color: `#663399`,
-//         display: `minimal-ui`,
-//         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-//       },
-//     },
-//   ],
-// }
-
-// module.exports = {
-//   siteMetadata: {
-//     title: "Gatsby Default Starter",
-//     description:
-//       "Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.",
-//     author: "@gatsbyjs",
-//     siteUrl: "https://tgibfnov2022.click/about-us/", // Replace with your site URL
-//   },
-//   plugins: [
-//     {
-//       resolve: "gatsby-source-wordpress",
-//       options: {
-//         // baseUrl: "http://dev1.kindlebit.com/Development/getsbysync/", // Your WordPress site URL
-//         url: "https://tgibfnov2022.click/about-us/", 
-//         protocol: "https",
-//         hostingWPCOM: false,
-//         useACF: false,
-//         verboseOutput: true,
-//         perPage: 100,
-//         debug: {
-//           graphql: {
-//             copyHtmlResponseOnError: true
-//           }
-//         },
-//         concurrentRequests: 10,
-//         includedRoutes: [
-//           "**/pages",
-//           "**/categories",
-//           "**/posts",
-//           "**/media",
-//           "**/tags",
-//           "**/taxonomies",
-//           "**/users",
-//         ],
-//       },
-//     },
-//     "gatsby-transformer-sharp",
-//     "gatsby-plugin-sharp",
-//     "gatsby-image",
-//   ],
-// };
 
 module.exports = {
   siteMetadata: {
@@ -90,17 +14,15 @@ module.exports = {
     description:
       "Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.",
     author: "mintu",
-    // siteUrl: "https://tgibfnov2022.click/about-us/", // Replace with your site URL
+   
   },
   plugins: [
     {
       resolve: "gatsby-source-wordpress",
       options: {
-        // baseUrl:"https://tgibfnov2022.click/",
-        // url: "https://wpgatsbydemo.wpengine.com/graphql",
-        // url: "https://tgibfnov2022.click/graphql",
+       
         url :"https://gatsbystarterdefaultsource.gatsbyjs.io/graphql",
-        // url:"https://tgibfnov2022.click/graphql",
+       
         protocol: "https",
         hostingWPCOM: false,
         useACF: false,
@@ -110,14 +32,6 @@ module.exports = {
           previewRequestConcurrency: 2,
         },
         verboseOutput: true,
-              
-        // perPage: 100,
-        // debug: {
-        //   graphql: {
-        //     copyHtmlResponseOnError: true,
-        //   },
-        // },
-        // concurrentRequests: 10,
         includedRoutes: [
           "**/about-us/",
           "**/pages",
@@ -136,11 +50,6 @@ module.exports = {
       options: {
         name: "images",
         path: `${__dirname}/src/images`,
-        // ignore: [
-        //   `**/uploads/**`, // Ignore any folder named "uploads" within the specified path
-        //   `**/*.@(jpg|jpeg|png|gif)`, // Ignore image files with jpg, jpeg, png, or gif extensions
-        //   `https://tgibfnov2022.click/**/*.@(jpg|jpeg|png|gif)`, // Ignore all images from the "tgibfnov2022.click" website
-        // ],
       },
     },
     {
@@ -148,13 +57,13 @@ module.exports = {
       options: {
         typeName: `WPGraphQL`,
         fieldName: `wpgraphql`,
-        // url: `https://tgibfnov2022.click/graphql`,
-         url: "https://wpgatsbydemo.wpengine.com/graphql", // Provide the base URL of your WordPress GraphQL API
+     
+         url: "https://wpgatsbydemo.wpengine.com/graphql",
       },
     },
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
-    // "gatsby-plugin-image",
+    "gatsby-plugin-image",
   ],
 };
 
--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -2,9 +2,7 @@ import React from "react";
 import { graphql } from "gatsby";
 
 const AboutUsPage = ({ data }) => {
-//   const page = data.allWordpressPage.nodes[0];
   const page = data?.wpgraphql?.pageBy;
-// const page = data?.wpgraphql?.postBy;
   console.log(data)
   if(!page){
     return(
@@ -19,37 +17,6 @@ const AboutUsPage = ({ data }) => {
     </div>
   );
 };
-
-// export const query = graphql`
-//   query {
-//     allWordpressPage(filter: { slug: { eq: "about-us" } }) {
-//       nodes {
-//         title
-//         content
-//       }
-//     }
-//   }
-// `;
- // wordpressPage(slug: { eq: "about-us" }) {
-// export const query = graphql`
-//   query AboutUsQuery {
-   
-//     wordpressPage(slug: {}) {
-//       title
-//       content
-//     }
-//   }
-// `;
-//      pageBy(uri: "https://crew-code.com/create-gatsby-wordpress-portfolio-website/") {
-    //   pageBy(uri: "/about-us/") {
-        //https://wpgraphqldemo.wpengine.com
-        //   postBy(slug: "fulvic-acid-facts") {
-            // "https://activatedprobiotics.oxdigital.com.au/graphql",
-            // /   const page = data.allWordpressPage.nodes[0];
-            // pageBy(uri: "https://tgibfnov2022.click/about-us") {
-              // https://gatsby-starter-blog-demo.netlify.app/
-//   const page = data?.wpgraphql?.pageBy;
-// const page = data?.wpgraphql?.postBy;
 export const query = graphql`
   query AboutUsQuery {
     wpgraphql {
